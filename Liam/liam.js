5f6266bfd59c36171b5df68005b7ae8564a91dc2
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-09-25 21:49:40 +0200

    add fadein and scroll animations

--- Liam/liam.js
+++ Liam/liam.js
@@ -1,6 +1,8 @@
 import ItemCarousel from "./itemCarousel.js";
 import { StarsBackground } from "./stars.js";
 import { initScrollAnimation } from "./animations.js";
+import { initAppearAnimation } from "./animations.js";
+import { delay } from "./helpers.js";
 
 function initItemCarousel() {
   const itemDiv = document.querySelector("#item-carousel");
@@ -18,13 +20,18 @@ function initStarsBackground() {
   starsBackground.init();
 }
 
-function initAnimations() {
+async function initAnimationsWithDelay(ms) {
+  await delay(ms);
+  initAppearAnimation();
+
+  await delay(2000)
   const span = document.querySelector("#scroll-span");
   initScrollAnimation(span);
 }
 
+
 initStarsBackground();
-initAnimations();
+initAnimationsWithDelay(100);
 
 /**
 
